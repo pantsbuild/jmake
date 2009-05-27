86f1555f674fa43c4aa63972c0e9a5614720a1da
barnett2 <barnett2@houston.westerngeco.slb.com>
2009-05-27 12:04:00 -0500

    Bump version to 1.3.4.  Version 1.3.4 contains support for JDK 6 class files.

--- src/com/sun/tools/jmake/Main.java
+++ src/com/sun/tools/jmake/Main.java
@@ -34,7 +34,7 @@ import java.util.List;
 public class Main {
 
     static final String DEFAULT_STORE_NAME = "jmake.pdb";
-    static final String VERSION = "1.3.3";
+    static final String VERSION = "1.3.4";
     private String pdbFileName = null;
     private List<String> allProjectJavaFileNamesList =
             new ArrayList<String>(100);
