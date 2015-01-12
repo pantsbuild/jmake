6c1243ae3fc88403c6a882ea7291c40df9a1ab19
David Turner <dturner@twitter.com>
2015-01-12 15:40:04 -0500

    jarjar, bump version

--- release.sh
+++ release.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+: ${JARJAR_JAR:=/usr/share/java/jarjar.jar}
+
+
 function die() {
     echo "$1"
     exit 1
@@ -13,6 +16,9 @@ ant package_for_release || die "failed to package"
 
 version=$(java -jar dist/release/jmake.jar -version 2>/dev/null | cut -d ' ' -f 3)
 
+# Use jarjar to rewrite jar:
+java -jar $JARJAR_JAR process rename.rules dist/release/jmake.jar dist/release/jmake-renamed.jar || die "jarjar failed"
+
 test -n "$version" || die "Could not get version"
 
 tag=$(git rev-parse HEAD)
@@ -21,7 +27,7 @@ cat jmake.pom | sed "s/%VERSION%/$version/" | sed "s/%TAG%/$tag/" > dist/release
 
 cd dist/release
 
-mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=ossrh -Dfile=jmake.jar -DgroupId=org.pantsbuild -DartifactId=jmake -Dversion=$version -DpomFile=jmake.pom || die "Failed to deploy jmake.jar"
+mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=ossrh -Dfile=jmake-renamed.jar -DgroupId=org.pantsbuild -DartifactId=jmake -Dversion=$version -DpomFile=jmake.pom || die "Failed to deploy jmake.jar"
 
 mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=ossrh -Dfile=jmake-sources.jar -DgroupId=org.pantsbuild -DartifactId=jmake -Dversion=$version -DpomFile=jmake.pom -Dclassifier=sources || die "Failed to deploy jmake-sources.jar"
 
